888075f80daf9f5f31bbb91ce33665974e8e3c43
Orasta Mirdadoeva <orasta@mra.com>
2021-11-19 17:12:49 +0500

    First add

---
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (UIElement el in GridName.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+            }
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+            if (str == "AC")
+            {
+                CalculatorText.Text = "";
+            }
+            else if (str == "=")
+            {
+                string value = new DataTable().Compute(CalculatorText.Text, null).ToString();
+                CalculatorText.Text = value;
+            }
+            else
+                CalculatorText.Text += str;
+            
+        }
+    }
+}
